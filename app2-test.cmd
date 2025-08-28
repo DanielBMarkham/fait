2c252cb5d2d3bb7c64cafb783e458fcc3ab6e8bd
Daniel Markham <danielbmarkham@hotmail.com>
2025-08-28 07:42:50 -0400

    forgot the batch commands

---
+++ app2-test.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM app2-test.cmd (DOS/Windows batch wrapper for app2-test.fsx)
+REM Run as: app2-test [options]
+
+dotnet fsi app2-test.fsx %*
---
+++ app2.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM app2.cmd (DOS/Windows batch wrapper for app2.fsx)
+REM Run as: app2 [options]
+
+dotnet fsi app2.fsx %*
